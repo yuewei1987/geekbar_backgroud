224dcfaad14e119c765634a8f8dd4ac195a0b06b
yue <418458884@qq.com>
2019-08-29 17:29:59 +0800

    增加用户扫码收货接口

--- web/controllers/api/Invoice.py
+++ web/controllers/api/Invoice.py
@@ -7,6 +7,7 @@
 """
 from  flask import request,jsonify,g
 
+from application import db
 from common.libs.Helper import getInvoiceDetail
 from common.models.invoice.Invoice import Invoice
 from web.controllers.api import route_api
@@ -21,4 +22,31 @@ def get_order():
         item = getInvoiceDetail(invoice)
         data.append(item)
     resp['data'] = [data]
+    return jsonify( resp )
+
+@route_api.route("/receiveInvoice", methods=["GET", "POST"])
+def receive_invoice():
+    resp = {'code': 200, 'msg': '操作成功~', 'data': {}}
+    req = request.values
+    invoice_id = req['invoice_id'] if 'invoice_id' in req else 0
+    if invoice_id == 0:
+        resp['code'] = -1
+        resp['msg'] = "ID不正确不能执行~~"
+        return jsonify(resp)
+    data = []
+    invoices = Invoice.query.filter(Invoice.del_flag == 0 and Invoice.invoice_id == invoice_id).first()
+    if not invoices :
+        resp['code'] = -1
+        resp['msg'] = "操作错误~~"
+        return jsonify(resp)
+    if invoices.status == 2:
+        resp['code'] = -1
+        resp['msg'] = "请不要重复操作~~"
+        return jsonify(resp)
+    if invoices:
+        invoices.status = 2
+        invoices.mid = g.member_info.id
+        invoices.group_name = g.member_info.group_name
+        db.session.add(invoices)
+        db.session.commit()
     return jsonify( resp )
